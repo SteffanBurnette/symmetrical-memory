311c4ab17a23204ce1dab87d6f2c41322155c0ff
Hady Bah <hadymbah@gmail.com>
2023-07-29 14:44:39 -0400

    college and major selection

--- UniHive/src/components/SingnUp.jsx
+++ UniHive/src/components/SingnUp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
@@ -6,9 +6,31 @@ import DialogContent from "@mui/material/DialogContent";
 import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
 
+// Sample college and major data from the JSON file
+import collegesData from "../../majors.json";
+
 function SignUp() {
   const [open, setOpen] = useState(false);
   const [collegeLevel, setCollegeLevel] = useState("");
+  const [colleges, setColleges] = useState([]);
+  const [selectedCollege, setSelectedCollege] = useState("");
+  const [majors, setMajors] = useState([]);
+  const [selectedMajor, setSelectedMajor] = useState("");
+
+  useEffect(() => {
+    // Fetch college data from the JSON file and set it in the state
+    setColleges(collegesData);
+  }, []);
+
+  useEffect(() => {
+    // Fetch majors based on the selected college and set them in the state
+    if (selectedCollege) {
+      const college = colleges.find((col) => col.college_Name === selectedCollege);
+      if (college) {
+        setMajors(college.college_Degrees);
+      }
+    }
+  }, [selectedCollege]);
 
   const handleOpen = () => {
     setOpen(true);
@@ -18,27 +40,29 @@ function SignUp() {
     setOpen(false);
   };
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     // Accessing the form elements by their id attributes and retrieving their values
     const fullName = document.getElementById("fullName").value;
-    const college = document.getElementById("college").value;
-    const major = document.getElementById("major").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     const confirmPassword = document.getElementById("confirmPassword").value;
-    
-
-
+    console.log("Full Name:", fullName);
+    console.log("Email:", email);
+    console.log("Password:", password);
+    console.log("Confirm Password:", confirmPassword);
+    console.log("College Level:", collegeLevel);
+    console.log("Selected College:", selectedCollege);
+    console.log("Selected Major:", selectedMajor);
   };
 
   return (
     <>
       <Button
         variant="outlined"
-        sx={{//styling
-          my: 10,//margin vertical
-          px:8,
+        sx={{
+          my: 10,
+          px: 8,
           marginRight: 8,
           borderWidth: "2px",
           color: "#FBCB1C",
@@ -68,24 +92,49 @@ function SignUp() {
           />
 
           <TextField
+            select
             id="college"
             label="College"
             variant="outlined"
             fullWidth
             margin="normal"
-            defaultValue="CUNY - College Of Staten Island"
-            InputProps={{
-              readOnly: true, // Prevents users from editing the input
-              sx: {
-                color: "gray", // Set the text color to gray
-              },
-            }}
-          />
+            value={selectedCollege}
+            onChange={(e) => setSelectedCollege(e.target.value)}
+          >
+            <MenuItem value="" disabled>
+              Select College
+            </MenuItem>
+            {colleges.map((college) => (
+              <MenuItem key={college.college_Name} value={college.college_Name}>
+                {college.college_Name}
+              </MenuItem>
+            ))}
+          </TextField>
 
-          {/* Replace the College Level TextField with the Select component */}
           <TextField
-            id = "collegeLevel"
             select
+            id="major"
+            label="Major"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={selectedMajor}
+            onChange={(e) => setSelectedMajor(e.target.value)}
+            disabled={!selectedCollege} // Disable until a college is selected
+          >
+            <MenuItem value="" disabled>
+              Select Major
+            </MenuItem>
+            {majors.map((major) => (
+              <MenuItem key={major} value={major}>
+                {major}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          <TextField
+            select
+            id="collegeLevel"
             label="College Level"
             variant="outlined"
             fullWidth
@@ -93,7 +142,9 @@ function SignUp() {
             value={collegeLevel}
             onChange={(e) => setCollegeLevel(e.target.value)}
           >
-            {/* Add the options for the dropdown menu */}
+            <MenuItem value="" disabled>
+              Select College Level
+            </MenuItem>
             <MenuItem value="Freshman">Freshman</MenuItem>
             <MenuItem value="Sophomore">Sophomore</MenuItem>
             <MenuItem value="Junior">Junior</MenuItem>
@@ -101,21 +152,14 @@ function SignUp() {
           </TextField>
 
           <TextField
-            id = "major"
-            label="Major"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-          />
-          <TextField
-            id = "email"
+            id="email"
             label="Email"
             variant="outlined"
             fullWidth
             margin="normal"
           />
           <TextField
-            id = "password"
+            id="password"
             label="Password"
             type="password"
             variant="outlined"
@@ -123,7 +167,7 @@ function SignUp() {
             margin="normal"
           />
           <TextField
-            id = "confirmPassword"
+            id="confirmPassword"
             label="Confirm Password"
             type="password"
             variant="outlined"
@@ -132,10 +176,155 @@ function SignUp() {
           />
         </DialogContent>
 
-        <Button sx={{ color: "black" }} onClick={handleSubmit}>Submit</Button>
+        <Button sx={{ color: "black" }} onClick={handleSubmit}>
+          Submit
+        </Button>
       </Dialog>
     </>
   );
 }
 
 export default SignUp;
+
+
+// import React, { useState } from "react";
+// import Button from "@mui/material/Button";
+// import Dialog from "@mui/material/Dialog";
+// import DialogTitle from "@mui/material/DialogTitle";
+// import DialogContent from "@mui/material/DialogContent";
+// import TextField from "@mui/material/TextField";
+// import MenuItem from "@mui/material/MenuItem";
+
+// function SignUp() {
+//   const [open, setOpen] = useState(false);
+//   const [collegeLevel, setCollegeLevel] = useState("");
+
+//   const handleOpen = () => {
+//     setOpen(true);
+//   };
+
+//   const handleClose = () => {
+//     setOpen(false);
+//   };
+
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+//     // Accessing the form elements by their id attributes and retrieving their values
+//     const fullName = document.getElementById("fullName").value;
+//     const college = document.getElementById("college").value;
+//     const major = document.getElementById("major").value;
+//     const email = document.getElementById("email").value;
+//     const password = document.getElementById("password").value;
+//     const confirmPassword = document.getElementById("confirmPassword").value;
+    
+
+
+//   };
+
+//   return (
+//     <>
+//       <Button
+//         variant="outlined"
+//         sx={{//styling
+//           my: 10,//margin vertical
+//           px:8,
+//           marginRight: 8,
+//           borderWidth: "2px",
+//           color: "#FBCB1C",
+//           borderColor: "#FBCB1C",
+//           "&:hover": {
+//             color: "black",
+//             borderColor: "#FBCB1C",
+//             backgroundColor: "#FBCB1C",
+//             borderWidth: "2px",
+//           },
+//         }}
+//         onClick={handleOpen}
+//       >
+//         Sign Up
+//       </Button>
+
+//       <Dialog open={open} onClose={handleClose} maxWidth="xs">
+//         <DialogTitle>Sign Up</DialogTitle>
+//         <DialogContent>
+//           {/* Add your input fields here */}
+//           <TextField
+//             id="fullName"
+//             label="Full Name"
+//             variant="outlined"
+//             fullWidth
+//             margin="normal"
+//           />
+
+//           <TextField
+//             id="college"
+//             label="College"
+//             variant="outlined"
+//             fullWidth
+//             margin="normal"
+//             defaultValue="CUNY - College Of Staten Island"
+//             InputProps={{
+//               readOnly: true, // Prevents users from editing the input
+//               sx: {
+//                 color: "gray", // Set the text color to gray
+//               },
+//             }}
+//           />
+
+//           {/* Replace the College Level TextField with the Select component */}
+//           <TextField
+//             id = "collegeLevel"
+//             select
+//             label="College Level"
+//             variant="outlined"
+//             fullWidth
+//             margin="normal"
+//             value={collegeLevel}
+//             onChange={(e) => setCollegeLevel(e.target.value)}
+//           >
+//             {/* Add the options for the dropdown menu */}
+//             <MenuItem value="Freshman">Freshman</MenuItem>
+//             <MenuItem value="Sophomore">Sophomore</MenuItem>
+//             <MenuItem value="Junior">Junior</MenuItem>
+//             <MenuItem value="Senior">Senior</MenuItem>
+//           </TextField>
+
+//           <TextField
+//             id = "major"
+//             label="Major"
+//             variant="outlined"
+//             fullWidth
+//             margin="normal"
+//           />
+//           <TextField
+//             id = "email"
+//             label="Email"
+//             variant="outlined"
+//             fullWidth
+//             margin="normal"
+//           />
+//           <TextField
+//             id = "password"
+//             label="Password"
+//             type="password"
+//             variant="outlined"
+//             fullWidth
+//             margin="normal"
+//           />
+//           <TextField
+//             id = "confirmPassword"
+//             label="Confirm Password"
+//             type="password"
+//             variant="outlined"
+//             fullWidth
+//             margin="normal"
+//           />
+//         </DialogContent>
+
+//         <Button sx={{ color: "black" }} onClick={handleSubmit}>Submit</Button>
+//       </Dialog>
+//     </>
+//   );
+// }
+
+// export default SignUp;
